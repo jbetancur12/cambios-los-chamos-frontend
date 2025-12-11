06e6dd42819c66fedfee7e2889eaf3d47993602d
Jorge Betancur <jabetancur12@gmail.com>
2025-12-11 14:11:04 -0500

    feat: implement web-vitals performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import reportWebVitals from './reportWebVitals'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 )
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+reportWebVitals((metric: any) => {
+  console.log(metric)
+})
---
+++ src/reportWebVitals.ts
@@ -0,0 +1,14 @@
+import { onCLS, onFID, onLCP, onINP, onFCP, onTTFB } from 'web-vitals'
+
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        onCLS(onPerfEntry)
+        onFID(onPerfEntry)
+        onLCP(onPerfEntry)
+        onINP(onPerfEntry)
+        onFCP(onPerfEntry)
+        onTTFB(onPerfEntry)
+    }
+}
+
+export default reportWebVitals
